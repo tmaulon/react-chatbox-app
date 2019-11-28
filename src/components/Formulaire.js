3d71c9a7ff17b02234a9347eb5ff6c7eaf3d84fa
Maulon <tmaulon@betomorrow.com>
2019-11-28 17:43:50 +0100

    begin handle submit and hanlde change messages

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
 export default class Formulaire extends Component {
+  state = {
+    message: ""
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("submit");
+  };
+  handleChange = event => {
+    const message = event.target.value;
+    this.setState({ message });
+  };
   render() {
     return (
-      <form className="form">
-        <textarea required maxLength="140" />
+      <form className="form" onSubmit={this.handleSubmit}>
+        <textarea
+          required
+          maxLength="140"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
         <div className="info">140</div>
         <button type="submit">Envoyer</button>
       </form>
