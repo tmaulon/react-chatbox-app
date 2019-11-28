37a934a03f162860cdae41bfae72efaa973e60a8
Maulon <tmaulon@betomorrow.com>
2019-11-28 17:34:24 +0100

    import formulaire component in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import Formulaire from "./components/Formulaire";
 
 class App extends Component {
   render() {
-    return <div className="box">Titre</div>;
+    return (
+      <div className="box">
+        <div>
+          <div className="messages">Messages</div>
+        </div>
+        <Formulaire />
+      </div>
+    );
   }
 }
 
