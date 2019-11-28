ef2938e9ab99cb773a4e581edce3a329325c4a46
Maulon <tmaulon@betomorrow.com>
2019-11-28 17:25:46 +0100

    add redirect on connexion form submit

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
+import { Redirect } from "react-router-dom";
+
 export default class Connexion extends Component {
+  state = {
+    pseudo: "",
+    goToChat: false
+  };
+
+  handleChange = event => {
+    const pseudo = event.target.value;
+    this.setState({ pseudo });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ goToChat: true });
+  };
+
   render() {
+    if (this.state.goToChat) {
+      return <Redirect to={`/pseudo/${this.state.pseudo}`} />;
+    }
+
     return (
       <div className="connexionBox">
-        <form action="" className="connexion">
-          <input type="text" placeholder="Pseudo" required />
+        <form action="" className="connexion" onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.pseudo}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Pseudo"
+            required
+          />
           <button type="submit">Se connecter</button>
         </form>
       </div>
