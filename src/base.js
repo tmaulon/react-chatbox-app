addcd1e6129117b4414e23c6497fc8f432cd0d4a
Maulon <tmaulon@betomorrow.com>
2019-11-29 17:08:35 +0100

    create  base for firebase

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from "re-base";
+import firebase from "firebbase/app";
+import "firebase/database";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDn0D3TaW14G_tRZmsUsTssZdUnanEIvMs",
+  authDomain: "react-chat-app-928ca.firebaseapp.com",
+  databaseURL: "https://react-chat-app-928ca.firebaseio.com"
+});
+
+const base = Rebase.createClass(firebase.database());
+
+export { firebaseApp };
+
+export default base;
