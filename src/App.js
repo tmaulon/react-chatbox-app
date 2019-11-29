7d4f1fdfab452cdd4e1988dad835dbcf09b709b0
Maulon <tmaulon@betomorrow.com>
2019-11-29 16:51:51 +0100

    add message component for display messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import Formulaire from "./components/Formulaire";
 
+import Message from "./components/Message";
+
 class App extends Component {
   state = {
     messages: {},
@@ -15,10 +17,22 @@ class App extends Component {
   };
 
   render() {
+    const messages = Object.keys(this.state.messages).map(key => (
+      <Message
+        key={key}
+        message={this.state.messages[key].message}
+        pseudo={this.state.messages[key].pseudo}
+      />
+    ));
+
+    console.log("messages => ", messages);
+
     return (
       <div className="box">
         <div>
-          <div className="messages">Messages</div>
+          <div className="messages">
+            <div className="message">{messages}</div>
+          </div>
         </div>
         <Formulaire
           length={140}
